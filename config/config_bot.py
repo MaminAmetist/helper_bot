4861fa718fcfc583ff2d0ce1540cf0bf6f83d626
Anna <anna.nezdulkina@mail.ru>
2025-11-24 14:19:54 +0700

    commit config

---
+++ config/config_bot.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class TGBot:
+    token: str
+
+
+@dataclass
+class Config:
+    tg_bot: TGBot
+
+
+def load_config(path: str | None = None) -> Config:
+    env: Env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TGBot(token=env('API')))
